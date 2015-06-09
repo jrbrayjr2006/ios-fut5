0099c6d4ce0467dc4646dae8ecda4d5508a4481f
James Bray <jrbrayjr2006@gmail.com>
2015-06-08 20:13:47 -0500

    Initial Commit

---
+++ Fut5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fut5
+//
+//  Created by JAMES BRAY on 6/8/15.
+//  Copyright (c) 2015 Jaydot2 LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
